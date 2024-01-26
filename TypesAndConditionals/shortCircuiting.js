5e0e2e7ce8675cb1f7c4b0cd9ef21d37e19dd103
Andrew Chu <andrew.c.chu@outlook.com>
2024-01-26 15:12:07 -0600

    Lecture complete

--- TypesAndConditionals/shortCircuiting.js
+++ TypesAndConditionals/shortCircuiting.js
@@ -1,1 +1,23 @@
 "use strict";
+
+//TODO: Shorten Conditionals with Short Circuiting
+
+
+/*Use logical operators && and || for short-circuit evaluation in conditions.
+Understand that && will stop at the first falsy value, and || will stop at the first truthy value.*/
+
+
+let user = null;
+let defaultUser = "Guest";
+
+let currentUser = user || defaultUser; // "Guest"
+console.log(currentUser);
+
+// Using && for a conditional action
+
+let isLoggedIn = true;
+
+
+isLoggedIn && console.log("User is logged in.");
+
+
--- TypesAndConditionals/ternaries.js
+++ TypesAndConditionals/ternaries.js
@@ -1,1 +1,17 @@
 "use strict";
+
+
+//TODO: Using Ternaries
+
+/*If/else statements can be repetitive. In order to conditionally assign a variable without repetition, we can use ternary operators:*/
+
+
+/*Use ternary operators for simple conditional assignments.
+Maintain readability; avoid overly complex ternary statements.*/
+
+
+let isLoggedIn = true;
+let message = isLoggedIn ? "Welcome back!" : "Please log in.";
+console.log(message);
+
+
