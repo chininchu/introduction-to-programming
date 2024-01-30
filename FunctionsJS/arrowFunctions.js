dda24864fc7758f14ddf9a9097506f01ed498c48
Andrew Chu <andrew.c.chu@outlook.com>
2024-01-30 14:57:05 -0600

    Creating lectures for Arrow Functions

---
+++ FunctionsJS/arrowFunctions.js
@@ -0,0 +1,25 @@
+"use strict";
+
+//TODO: Arrow Functions
+
+
+/*Brief Overview: Introduce the concept of Arrow functions in JavaScript, a feature introduced in ES6 (ECMAScript 2015) as a new syntax for writing function expressions.
+*/
+
+// Traditional Function Expression
+// const add = function(a, b) {
+//     return a + b;
+// };
+
+// Arrow Function
+// const add = (a, b) => a + b;
+
+
+/*Concise Syntax: Explain how arrow functions allow for shorter function expressions.
+
+Implicit Return: In cases of a single expression, {} and return are optional.*/
+
+//Example
+// const square = n => n * n;
+
+
