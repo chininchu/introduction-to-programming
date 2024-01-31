5b9c959b128ed9b151829626459720d554dcb134
Andrew Chu <andrew.c.chu@outlook.com>
2024-01-31 16:23:33 -0600

    Created lecture for partial applications for single respo functions

---
+++ FunctionsJS/partialApplicationforSingle-ResponsibilityFunctions.js
@@ -0,0 +1,45 @@
+"use strict";
+
+
+//TODO: Partial Application for Single-Responsibility Functions
+
+// Importance
+
+/*Enhances Reusability: Partial application allows for the creation of more specific functions from more general ones, increasing reusability.
+
+Encourages Single Responsibility: Functions can be designed to do one thing and do it well, aligning with the Single Responsibility Principle (SRP).
+
+
+ */
+
+// The goal of a function is to focus on a single task
+
+
+// General Credit Score Evaluation Function
+
+// First, define a general function that checks if the credit score meets the minimum required score for a loan:
+
+function isEligibleForLoan(minCreditScore, creditScore) {
+    return creditScore >= minCreditScore;
+}
+
+
+// Partial Application for Loan Eligibility
+
+// Using partial application, you can create functions for specific loan types based on their minimum credit score requirements:
+
+
+function createLoanEligibilityChecker(minCreditScore) {
+    return function (creditScore) {
+        return isEligibleForLoan(minCreditScore, creditScore);
+    };
+}
+
+const isEligibleForPersonalLoan = createLoanEligibilityChecker(600);
+const isEligibleForMortgage = createLoanEligibilityChecker(650);
+const isEligibleForAutoLoan = createLoanEligibilityChecker(580);
+
+
+
+
+
