d940e932daa571d04a7f274ea1a42e99750655b5
Andrew Chu <andrew.c.chu@outlook.com>
2024-02-06 16:32:29 -0600

    Created Arrays_01_introduction slides

---
+++ LoopsAndArraysJS/jsArrays_01_introduction.js
@@ -0,0 +1,11 @@
+"use strict";
+/*Definition: An array is a collection of data items stored under a single name. Each item in an array is called an element, and each element is accessed by its numeric index, starting with zero.*/
+
+
+const fruits = ['Apple', 'Banana', 'Cherry'];
+console.log(fruits);
+
+
+/*Real-Life Analogy: Think of an array like a row of mailboxes, each with its own number and containing a message or data.*/
+
+
