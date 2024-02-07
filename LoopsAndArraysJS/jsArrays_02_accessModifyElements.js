18618616b4df989807b2f192169836c3921e3f94
Andrew Chu <andrew.c.chu@outlook.com>
2024-02-07 12:05:39 -0600

    Created lecture 2 slides. Access and Modify Elements

--- LoopsAndArraysJS/jsArrays_02_accessModifyElements.js
+++ LoopsAndArraysJS/jsArrays_02_accessModifyElements.js
@@ -1,1 +1,10 @@
 "use strict";
+/*3. Accessing Array Elements
+
+How-To: You can access any element in an array by using its index, with the first element at index 0.*/
+
+console.log(fruits[0]); // Outputs: Apple
+
+/*Accessing elements by index is fundamental to working with arrays, whether it's reading a value or using it in a function.
+
+*/
