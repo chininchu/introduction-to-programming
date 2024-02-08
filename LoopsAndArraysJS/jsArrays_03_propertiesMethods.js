1160e434651b94a5b0a332b022081cbac28dabc4
Andrew Chu <andrew.c.chu@outlook.com>
2024-02-08 12:26:11 -0600

    Completed the JS Properties and Methods lecture

--- LoopsAndArraysJS/jsArrays_03_propertiesMethods.js
+++ LoopsAndArraysJS/jsArrays_03_propertiesMethods.js
@@ -13,9 +13,9 @@ const fruits = ['Apple', 'Banana', 'Cherry'];
 console.log(fruits.length); // Outputs: 3
 
 
-/*Methods: push(), pop(), shift(), unshift(), slice(), forEach()
+/*Methods: push(), pop(), shift(), unshift(), slice()
 
-Arrays come with a variety of methods that make it easy to add, remove, and manipulate elements.*/
+Arrays come with a variety of methods that make it easy to add, remove, and manipulate elements. There are many more built in methods that are chained with the array data type. */
 
 // Push and Pop methods
 
@@ -24,6 +24,24 @@ console.log(fruits); // ['Apple', 'Banana', 'Cherry', 'Dragonfruit']
 
 fruits.pop();
 console.log(fruits); // ['Apple', 'Banana', 'Cherry']
+
+
+// shift() / unshift(): Remove or add elements to the beginning of an array.
+
+fruits.shift();
+console.log(fruits); // ['Banana', 'Cherry']
+
+fruits.unshift('Strawberry');
+console.log(fruits); // ['Strawberry', 'Banana', 'Cherry']
+
+// slice(): Returns a copy of a portion of an array.
+
+const someFruits = fruits.slice(1, 2);
+console.log(someFruits); // ['Banana']
+
+
+
+
 
 
 
