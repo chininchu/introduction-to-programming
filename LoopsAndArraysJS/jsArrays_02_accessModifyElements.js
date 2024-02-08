3359335bb4dc8cd1fd60239b6692a3cc18b15d50
Andrew Chu <andrew.c.chu@outlook.com>
2024-02-08 12:34:10 -0600

    Added a few more content to the the jsArrays_02_accessModifyElements file

--- LoopsAndArraysJS/jsArrays_02_accessModifyElements.js
+++ LoopsAndArraysJS/jsArrays_02_accessModifyElements.js
@@ -3,9 +3,26 @@
 
 How-To: You can access any element in an array by using its index, with the first element at index 0.*/
 
+const fruits = ['Apple', 'Banana', 'Cherry'];
+
 console.log(fruits[0]); // Outputs: Apple
+
+console.log(fruits[1]); // Outputs: Banana
+
 
 /*Accessing elements by index is fundamental to working with arrays, whether it's reading a value or using it in a function.
 
 */
+
+
+/*Changing an Array Element */
+
+
+// Change the second fruit
+fruits[1] = 'Blueberry';
+console.log(fruits); // Outputs: ['Apple', 'Blueberry', 'Cherry']
+
+
+
+
 
