71c79e180e8eb2627eece1c2df9ef0b88eabce10
Andrew Chu <andrew.c.chu@outlook.com>
2024-02-08 13:59:56 -0600

    Added the some and every method examples

--- LoopsAndArraysJS/jsArrays_03_propertiesMethods.js
+++ LoopsAndArraysJS/jsArrays_03_propertiesMethods.js
@@ -13,7 +13,7 @@ const fruits = ['Apple', 'Banana', 'Cherry'];
 console.log(fruits.length); // Outputs: 3
 
 
-/*Methods: push(), pop(), shift(), unshift(), slice()
+/*Methods: push(), pop(), shift(), unshift(), slice(), some(), every()
 
 Arrays come with a variety of methods that make it easy to add, remove, and manipulate elements. There are many more built in methods that are chained with the array data type. */
 
@@ -38,6 +38,51 @@ console.log(fruits); // ['Strawberry', 'Banana', 'Cherry']
 
 const someFruits = fruits.slice(1, 2);
 console.log(someFruits); // ['Banana']
+
+
+// The some method tests whether at least one element in the array passes the test implemented by the provided function. It returns a Boolean value (true or false).
+
+
+// Syntax:  arr.some(callback(element[, index[, array]])[, thisArg])
+
+
+// Let's check if atleast one of these numbers in this array are even numbers.
+
+
+const listOfNum = [1, 2, 3, 4, 5, 6];
+
+
+const evenNum = listOfNum.some((number) => {
+
+    return number % 2 === 0;
+
+
+});
+
+console.log(evenNum);
+
+
+// The every method tests whether all elements in the array pass the test implemented by the provided function. Like some, it returns a Boolean value.
+
+// Syntax: arr.every(callback(element[, index[, array]])[, thisArg])
+
+// Let's check if all the numbers are positive
+
+
+const posNum = listOfNum.some((number) => {
+
+    return number % 2 > 0;
+
+
+});
+
+console.log(posNum);
+
+
+
+
+
+
 
 
 
