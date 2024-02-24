414c84500501fc3f0119938e065b09a2e7a5c91c
Andrew Chu <andrew.c.chu@outlook.com>
2024-02-24 22:11:05 +0500

    1:1 mentor with CTD student

--- LoopsAndArraysJS/jsArrays_04_loopingOverArrays.js
+++ LoopsAndArraysJS/jsArrays_04_loopingOverArrays.js
@@ -1,72 +1,93 @@
-"use strict";
-
-/*Loops
-
-Loops offer a powerful way to iterate over an array, performing operations on each element, from simple tasks like logging values to complex data processing.
-
-
-*/
-
-
-// Example of a for loop in JavaScript
-
-
-// for (let i = 0; i < 5; i++) {
-//     console.log("The value of i is: " + i);
-// }
-
-/*let i = 0 initializes the loop variable i to 0.
-
-i < 5 is the condition that the loop will continue to run as long as i is less than 5.
-
-i++ is the increment statement, which increases the value of i by 1 in each iteration.
-
-console.log("The value of i is: " + i) logs the current value of i to the console in each iteration.*/
-
-
-// Example og a for loop in JS over an array
-
-
-// Example of a for loop iterating over an array
-
-
-// for (let i = 0; i < fruits.length; i+=2) {
-//     console.log("Fruit at index " + i + " is: " + fruits[i]);
-// }
-
-
-// function repeatAStr(num) {
-//     //then using either a for or while loop, it will print out the string "Hello World!" to the console the amount of times as the parameter
-//     //output is a 'str'
+// "use strict";
 //
-//     for (let i = 0; i <= num; i++) {
-//         console.log('Hello World!');
-//     }
+// /*Loops
+//
+// Loops offer a powerful way to iterate over an array, performing operations on each element, from simple tasks like logging values to complex data processing.
+//
+//
+// */
+//
+//
+// // Example of a for loop in JavaScript
+//
+//
+// // for (let i = 0; i < 5; i++) {
+// //     console.log("The value of i is: " + i);
+// // }
+//
+// /*let i = 0 initializes the loop variable i to 0.
+//
+// i < 5 is the condition that the loop will continue to run as long as i is less than 5.
+//
+// i++ is the increment statement, which increases the value of i by 1 in each iteration.
+//
+// console.log("The value of i is: " + i) logs the current value of i to the console in each iteration.*/
+//
+//
+// // Example og a for loop in JS over an array
+//
+//
+// // Example of a for loop iterating over an array
+//
+//
+// // for (let i = 0; i < fruits.length; i+=2) {
+// //     console.log("Fruit at index " + i + " is: " + fruits[i]);
+// // }
+//
+//
+// // function repeatAStr(num) {
+// //     //then using either a for or while loop, it will print out the string "Hello World!" to the console the amount of times as the parameter
+// //     //output is a 'str'
+// //
+// //     for (let i = 0; i <= num; i++) {
+// //         console.log('Hello World!');
+// //     }
+// // }
+// //
+// // repeatAStr(3);
+//
+//
+// function removeVowels(str) {
+//
+//
+//     return str.replace(/[aeiouAEIOU]/g, "");
+//
+//
 // }
 //
-// repeatAStr(3);
+// const originalString = "Andrew";
+//
+// const stringWithoutVowels = removeVowels(originalString);
+//
+//
+// console.log(stringWithoutVowels);
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 
-function removeVowels(str) {
+const users = [
+    {name: 'Alice', age: 20},
+    {name: 'Bob', age: 17},
+    {name: 'Charlie', age: 23},
+    {name: 'David', age: 16}
+];
+
+const adults = users.filter((user) => {
+
+    return user.name.includes("Alice");
 
 
-    return str.replace(/[aeiouAEIOU]/g, "");
+});
+// console.log(adults);
 
 
-}
-
-const originalString = "Andrew";
-
-const stringWithoutVowels = removeVowels(originalString);
-
-
-console.log(stringWithoutVowels);
-
-
-
-
-
-
-
-
-
+const numbers = [1, 2, 3, 4];
+const timesTwo = numbers.map(number => number * 2);
+console.log(timesTwo);
