498d5d8e2887492f0cfa94dd58ca6b607d658242
Andrew Chu <andrew.c.chu@outlook.com>
2024-04-18 19:30:56 +0500

    Added comments

--- DOM/script.js
+++ DOM/script.js
@@ -1,5 +1,9 @@
 "use strict";
 
+//************************************
+// 2. Get Single and Multiple Elements
+//************************************
+
 // Get Single Element
 const mainTitle = document.getElementById('main-title');
 console.log(mainTitle);
@@ -7,6 +11,11 @@ console.log(mainTitle);
 // Get Multiple Elements
 const descriptionParagraphs = document.getElementsByClassName('description');
 console.log(descriptionParagraphs);
+
+
+//************************************
+// 3.Creating and Modifying HTML elements
+//************************************
 
 
 // Create a new element
@@ -21,13 +30,21 @@ const myList = document.getElementById('my-list');
 myList.appendChild(newListItem);
 
 
+//************************************
+// 4.Dynamically Adding Styles
+//************************************
+
+
 // Add a class to an element for styling
 const changeTextButton = document.getElementById('change-text');
 changeTextButton.classList.add('highlight');
 
-// Events
+//************************************
+// 5.Understanding and Working with Events
+//************************************
 
-changeTextButton.addEventListener('click', function() {
+
+changeTextButton.addEventListener('click', function () {
     const firstParagraph = document.querySelector('.description');
     firstParagraph.textContent = 'The button was clicked!';
 });
